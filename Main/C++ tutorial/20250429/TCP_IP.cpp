c087a4218ad18c9518c6acd9a9b0b7d58bba56ac
jhpark <jhpark@intellivix.ai>
2025-04-30 18:01:20 +0900

    20250430_daily_commit

--- Main/C++ tutorial/20250429/TCP_IP.cpp
+++ Main/C++ tutorial/20250429/TCP_IP.cpp
@@ -1,1 +1,29 @@
-#define PORT 8080
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define PACKET_SIZE 1024;
+
+#include <iostream>
+#include <WinSock2.h>
+#include <thread>
+using namespace std;
+
+#pragma comment(lib,"ws2_32.lib")
+
+SOCKET server_socket, client_socket;
+
+int TCP_IP_test() {
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+		cout << "WSAStartup failed" << endl;
+		return 1;
+	}
+
+	server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	SOCKADDR_IN addr = {};
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(4444);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	WSACleanup();
+	return 0;
+}
